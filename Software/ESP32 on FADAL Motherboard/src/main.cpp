24ef8f0c458b6c922abf8f73530ed254c7770c22
blaisebarrette <53702710+blaisebarrette@users.noreply.github.com>
2022-02-05 15:44:23 -0500

    update

--- Software/ESP32 on FADAL Motherboard/src/main.cpp
+++ Software/ESP32 on FADAL Motherboard/src/main.cpp
@@ -1,35 +1,35 @@
 /*################ Libraries ################*/
   #include <Arduino.h>
   #include <ModbusRTU.h> // https://github.com/emelianov/modbus-esp8266
-  //#include <OneWire.h> 
-  //#include <DallasTemperature.h>
+  #include <OneWire.h> 
+  #include <DallasTemperature.h>
 
 /*################ ESP32 Pin asignement ################*/
   //--- Outputs ----//
-    #define MK2_10 5
-    #define MK2_11 18
-    #define MK2_12 13
-    #define MK2_13 12
-    #define MK2_14 14
-    #define MK2_15 27
-    #define MK2_16 26
+    #define MK2_6_pin 13
+    #define MK2_7_pin 12
+    #define MK2_8_pin 14
+    #define MK2_9_pin 27
+    #define MK2_10_pin 26
+    #define MK2_11_pin 25
+    #define MK2_12_pin 33
 
   //--- Inputs ----//
-    #define Charge_pump 2
-    #define Temperatur_sensors 22
+    #define Charge_pump_pin 2
+    #define Temperatur_sensors_pin 22
 
   //--- RS485 ----//
-    #define RS485_TX 17
-    #define RS485_RX 16
-    #define RS485_RE_DE 4
+    #define RS485_TX_pin 17
+    #define RS485_RX_pin 16
+    #define RS485_RE_DE_pin 4
 
 /*################ Constant ################*/
   #define SLAVE_ID 2 // Used for modbus
 
 /*################ Object Initialisation ################*/
   ModbusRTU mb; // Modbus Initialisation //
-  //OneWire oneWire(Temperatur_sensors);
-  //DallasTemperature sensors(&oneWire);
+  OneWire oneWire(Temperatur_sensors_pin);
+  DallasTemperature sensors(&oneWire);
 
 /*################ Variables ################*/
   bool Debuging_Mode = false;
@@ -41,43 +41,48 @@
   int Current_Charge_pump_value;
 
 /*################ Functions ################*/
+  //------------------- Write charge pump value to modbus -------------------//
+    void Charge_pump(){
+      Current_Charge_pump_value = analogRead(Charge_pump_pin);
+      mb.Hreg(150, Current_Charge_pump_value);
+    }
   //------------------- Debug -------------------//
     void Debug(){
       unsigned long currentMillis = millis();
       if (currentMillis - previousMillis >= 250) {
         previousMillis = currentMillis;
-        Serial.printf("Air temperature = %i Chiller temperature = %i Charge Pump = %i \n", Current_Temp_air, Current_Temp_chiller, Current_Charge_pump_value);
+        Serial.printf("Air temperature = %d Chiller temperature = %d Charge Pump = %i \n", Current_Temp_air, Current_Temp_chiller, Current_Charge_pump_value);
       }
     }
 
 void setup() {
   // Pin Modes //
-    pinMode(MK2_10, OUTPUT);
-    pinMode(MK2_11, OUTPUT);
-    pinMode(MK2_12, OUTPUT);
-    pinMode(MK2_13, OUTPUT);
-    pinMode(MK2_14, OUTPUT);
-    pinMode(MK2_15, OUTPUT);
-    pinMode(MK2_16, OUTPUT);
+    pinMode(MK2_6_pin, OUTPUT);
+    pinMode(MK2_7_pin, OUTPUT);
+    pinMode(MK2_8_pin, OUTPUT);
+    pinMode(MK2_9_pin, OUTPUT);
+    pinMode(MK2_10_pin, OUTPUT);
+    pinMode(MK2_11_pin, OUTPUT);
+    pinMode(MK2_12_pin, OUTPUT);
 
-    pinMode(Charge_pump, INPUT);
-    pinMode(Temperatur_sensors, INPUT);
+    pinMode(Charge_pump_pin, INPUT);
+    pinMode(Temperatur_sensors_pin, INPUT);
 
-    digitalWrite(MK2_10,LOW);
-    digitalWrite(MK2_11,LOW);
-    digitalWrite(MK2_12,LOW);
-    digitalWrite(MK2_13,LOW);
-    digitalWrite(MK2_14,LOW);
-    digitalWrite(MK2_15,LOW);
-    digitalWrite(MK2_16,LOW);
+    digitalWrite(MK2_6_pin,LOW);
+    digitalWrite(MK2_7_pin,LOW);
+    digitalWrite(MK2_8_pin,LOW);
+    digitalWrite(MK2_9_pin,LOW);
+    digitalWrite(MK2_10_pin,LOW);
+    digitalWrite(MK2_11_pin,LOW);
+    digitalWrite(MK2_12_pin,LOW);
 
   // Temperature sensor //
-    //Serial.begin(115200);
-    //sensors.begin();
+    Serial.begin(115200);
+    sensors.begin();
 
   // Modbus stuf //
-    Serial1.begin(115200, SERIAL_8N1, RS485_RX, RS485_TX);
-    mb.begin(&Serial1, RS485_RE_DE);
+    Serial1.begin(115200, SERIAL_8N1, RS485_RX_pin, RS485_TX_pin);
+    mb.begin(&Serial1, RS485_RE_DE_pin);
     mb.slave(SLAVE_ID);
 
   /*################ From Master to slave ################*/
@@ -87,21 +92,20 @@ void setup() {
       mb.addHreg(150);
 
       hreg100 = mb.Hreg(100);
-      mb.Hreg(150, 100); // initially set the value to 100% on modbus
 }
 
 void loop() {
   // Get current temperature
-    /*sensors.requestTemperatures();
+    sensors.requestTemperatures();
     Current_Temp_air = sensors.getTempCByIndex(0); //Ambiant air
-    Current_Temp_chiller = sensors.getTempCByIndex(1); // Chiller*/
-
-  Current_Charge_pump_value = analogRead(Charge_pump);
+    Current_Temp_chiller = sensors.getTempCByIndex(1); // Chiller
+    
   // Update Mudbus
     mb.task();
     yield();
 
   // Call functions
+    Charge_pump();
     if (Debuging_Mode){ Debug(); }
 
   // One milisecond delay for smoot operation
